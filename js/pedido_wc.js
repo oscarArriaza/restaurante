90a4bd9e0cf735bedb1d02bc1479dc76738a5095
oscarArriaza <arriaza22598@vk.com>
2024-06-13 04:23:27 +0200

    oscar

--- js/pedido_wc.js
+++ js/pedido_wc.js
@@ -1,123 +1,125 @@
 document.addEventListener("DOMContentLoaded", function() {
-  class PedidoElement extends HTMLElement {
-      constructor() {
-          super();
+    class PedidoElement extends HTMLElement {
+        constructor() {
+            super();
+            
+            if (!this.shadowRoot) {
+                // Creamos el shadow DOM solo si no existe
+                this.attachShadow({ mode: 'open' });
+                this.platos = [];
 
-          // Creamos el shadow DOM
-          this.shadow = this.attachShadow({ mode: 'open' });
-          this.platos = [];
+                // Definimos la template (HTML + CSS)
+                let template = document.createElement('template');
+                template.innerHTML = `
+                    <style>
+                        @import url('./bootstrap/css/bootstrap.css');
+                    </style>
+                    <p class="text-success fw-bold">
+                        "No se han agregado platos al pedido."
+                    </p>
+                `;
+                
+                // Añadimos el contenido de la template (clonándola) al shadow DOM
+                this.shadowRoot.appendChild(template.content.cloneNode(true));
+            }
+        }
 
-          // Definimos la template (HTML + CSS)
-          this.template = document.createElement('template');
-          this.template.innerHTML = `
-              <style>
-                  @import url('./bootstrap/css/bootstrap.css');
-              </style>
-              <p class="text-success fw-bold">
-                  "No se han agregado platos al pedido."
-              </p>
-          `;
+        connectedCallback() {
+            window.addEventListener('add-plato', (event) => {
+                let { idPlato, nombrePlato, precioPlato } = event.detail;
 
-          // Añadimos el contenido de la template (clonándola) al shadow DOM
-          this.shadow.appendChild(this.template.content.cloneNode(true));
-      }
+                let platoExistente = this.platos.find(plate => plate.getAttribute("data-id-plato") === idPlato);
+                if (platoExistente === undefined) {
+                    let platoPedido = document.createElement('plato-pedido-element');
+                    platoPedido.setAttribute("data-id-plato", idPlato);
+                    platoPedido.setAttribute("data-nombre-plato", nombrePlato);
+                    platoPedido.setAttribute("data-precio-plato-unidad", precioPlato);
+                    platoPedido.setAttribute("data-precio-plato-total", precioPlato);
+                    platoPedido.setAttribute("data-cantidad-plato", 1);
+                    platoPedido.setAttribute("class", "platoDePedido");
 
-      connectedCallback() {
-          window.addEventListener('add-plato', (event) => {
-              let { idPlato, nombrePlato, precioPlato } = event.detail;
+                    this.platos.push(platoPedido);
+                    this.updateTemplate();
+                } else {
+                    this.actualizarPlatoExistente(platoExistente, 1);
+                }
+            });
 
-              let platoExistente = this.platos.find(plate => plate.getAttribute("data-id-plato") === idPlato);
-              if (platoExistente === undefined) {
-                  let platoPedido = document.createElement('plato-pedido-element');
-                  platoPedido.setAttribute("data-id-plato", idPlato);
-                  platoPedido.setAttribute("data-nombre-plato", nombrePlato);
-                  platoPedido.setAttribute("data-precio-plato-unidad", precioPlato);
-                  platoPedido.setAttribute("data-precio-plato-total", precioPlato);
-                  platoPedido.setAttribute("data-cantidad-plato", 1);
-                  platoPedido.setAttribute("class", "platoDePedido");
+            window.addEventListener('add-plato-del-mismo-tipo', (event) => {
+                let { idPlato, cantidadPlato } = event.detail;
+                let plato = this.platos.find(plate => plate.getAttribute("data-id-plato") === idPlato);
 
-                  this.platos.push(platoPedido);
-                  this.updateTemplate();
-              } else {
-                  this.actualizarPlatoExistente(platoExistente, 1);
-              }
-          });
+                this.actualizarPlatoExistente(plato, cantidadPlato - plato.getAttribute("data-cantidad-plato"));
+            });
 
-          window.addEventListener('add-plato-del-mismo-tipo', (event) => {
-              let { idPlato, cantidadPlato } = event.detail;
-              let plato = this.platos.find(plate => plate.getAttribute("data-id-plato") === idPlato);
+            window.addEventListener('restar-plato-del-mismo-tipo', (event) => {
+                let { idPlato, cantidadPlato } = event.detail;
+                let plato = this.platos.find(plate => plate.getAttribute("data-id-plato") === idPlato);
 
-              this.actualizarPlatoExistente(plato, cantidadPlato - plato.getAttribute("data-cantidad-plato"));
-          });
+                this.actualizarPlatoExistente(plato, cantidadPlato - plato.getAttribute("data-cantidad-plato"));
+            });
+        }
 
-          window.addEventListener('restar-plato-del-mismo-tipo', (event) => {
-              let { idPlato, cantidadPlato } = event.detail;
-              let plato = this.platos.find(plate => plate.getAttribute("data-id-plato") === idPlato);
+        actualizarPlatoExistente(plato, incremento) {
+            let cantidadActual = parseInt(plato.getAttribute("data-cantidad-plato"));
+            let nuevaCantidad = cantidadActual + incremento;
 
-              this.actualizarPlatoExistente(plato, cantidadPlato - plato.getAttribute("data-cantidad-plato"));
-          });
-      }
+            if (nuevaCantidad <= 0) {
+                this.platos = this.platos.filter(p => p !== plato);
+            } else {
+                plato.setAttribute("data-cantidad-plato", nuevaCantidad);
+                plato.setAttribute("data-precio-plato-total", parseFloat(plato.getAttribute("data-precio-plato-unidad")) * nuevaCantidad);
+            }
+            this.updateTemplate();
+        }
 
-      actualizarPlatoExistente(plato, incremento) {
-          let cantidadActual = parseInt(plato.getAttribute("data-cantidad-plato"));
-          let nuevaCantidad = cantidadActual + incremento;
+        updateTemplate() {
+            this.shadowRoot.innerHTML = '';
+            let template = document.createElement('template');
 
-          if (nuevaCantidad <= 0) {
-              this.platos = this.platos.filter(p => p !== plato);
-          } else {
-              plato.setAttribute("data-cantidad-plato", nuevaCantidad);
-              plato.setAttribute("data-precio-plato-total", parseFloat(plato.getAttribute("data-precio-plato-unidad")) * nuevaCantidad);
-          }
-          this.updateTemplate();
-      }
+            if (this.platos.length === 0) {
+                template.innerHTML = `
+                    <style>
+                        @import url('./bootstrap/css/bootstrap.css');
+                    </style>
+                    <p class="text-success fw-bold">
+                        "No se han agregado platos al pedido."
+                    </p>
+                `;
+            } else {
+                template.innerHTML = `
+                    <style>
+                        @import url('./bootstrap/css/bootstrap.css');
+                    </style>
+                    <div class="container">
+                        <ul class="list-group"></ul>
+                    </div>
+                    <div class="container">
+                        <ul class="list-group">
+                            <li class="list-group-item mb-2">
+                                <div class="d-flex justify-content-end">
+                                    <span id="total" class="text-light"></span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                `;
 
-      updateTemplate() {
-          this.shadow.innerHTML = '';
-          let template = document.createElement('template');
+                let list = template.content.querySelector('ul');
+                let spantotal = template.content.querySelector('#total');
+                let total = 0;
 
-          if (this.platos.length === 0) {
-              template.innerHTML = `
-                  <style>
-                      @import url('./bootstrap/css/bootstrap.css');
-                  </style>
-                  <p class="text-success fw-bold">
-                      "No se han agregado platos al pedido."
-                  </p>
-              `;
-          } else {
-              template.innerHTML = `
-                  <style>
-                      @import url('./bootstrap/css/bootstrap.css');
-                  </style>
-                  <div class="container">
-                      <ul class="list-group"></ul>
-                  </div>
-                  <div class="container">
-                      <ul class="list-group">
-                          <li class="list-group-item mb-2">
-                              <div class="d-flex justify-content-end">
-                                  <span id="total" class="text-light"></span>
-                              </div>
-                          </li>
-                      </ul>
-                  </div>
-              `;
+                this.platos.forEach(plato => {
+                    list.appendChild(plato);
+                    total += Number(plato.getAttribute("data-precio-plato-total"));
+                });
 
-              let list = template.content.querySelector('ul');
-              let spantotal = template.content.querySelector('#total');
-              let total = 0;
+                spantotal.innerHTML = "Total: " + total + " €";
+            }
 
-              this.platos.forEach(plato => {
-                  list.appendChild(plato);
-                  total += Number(plato.getAttribute("data-precio-plato-total"));
-              });
+            this.shadowRoot.appendChild(template.content.cloneNode(true));
+        }
+    }
 
-              spantotal.innerHTML = "Total: " + total + " €";
-          }
-
-          this.shadow.appendChild(template.content.cloneNode(true));
-      }
-  }
-
-  customElements.define('pedido-element', PedidoElement);
+    customElements.define('pedido-element', PedidoElement);
 });
--- js/plato_pedido_wc.js
+++ js/plato_pedido_wc.js
@@ -1,86 +1,88 @@
 document.addEventListener("DOMContentLoaded", function() {
-  class PlatoPedidoElement extends HTMLElement {
-      constructor() {
-          super();
+    class PlatoPedidoElement extends HTMLElement {
+        constructor() {
+            super();
 
-          // Creamos el shadow DOM
-          this.shadow = this.attachShadow({ mode: 'open' });
+            if (!this.shadowRoot) {
+                // Creamos el shadow DOM solo si no existe
+                this.attachShadow({ mode: 'open' });
 
-          // Recuperamos los atributos
-          this.idPlato = this.getAttribute("data-id-plato") || "Id del plato desconocido.";
-          this.nombrePlato = this.getAttribute("data-nombre-plato") || "Nombre del plato desconocido.";
-          this.precioPlatoTotal = this.getAttribute("data-precio-plato-total") || 'Precio del plato total desconocido.';
-          this.cantidad = this.getAttribute("data-cantidad-plato") || 'Cantidad del plato desconocida.';
+                // Recuperamos los atributos
+                this.idPlato = this.getAttribute("data-id-plato") || "Id del plato desconocido.";
+                this.nombrePlato = this.getAttribute("data-nombre-plato") || "Nombre del plato desconocido.";
+                this.precioPlatoTotal = this.getAttribute("data-precio-plato-total") || 'Precio del plato total desconocido.';
+                this.cantidad = this.getAttribute("data-cantidad-plato") || 'Cantidad del plato desconocida.';
 
-          // Definimos la template (HTML + CSS)
-          this.template = document.createElement('template');
-          this.template.innerHTML = `
-              <style>
-                  @import url('./bootstrap/css/bootstrap.css');
-              </style>
-              <li class="list-group-item mb-2">
-                  <div class="d-flex justify-content-between">
-                      <span>${this.nombrePlato}</span>
-                      <button type="button" class="btn btn-sm btn-danger botonQuitar">-</button>
-                      <span class="cantidad">${this.cantidad}</span>
-                      <button type="button" class="btn btn-sm btn-success botonAniadir">+</button>
-                      <span class="precio">${this.precioPlatoTotal} €</span>
-                  </div>
-              </li>
-          `;
+                // Definimos la template (HTML + CSS)
+                let template = document.createElement('template');
+                template.innerHTML = `
+                    <style>
+                        @import url('./bootstrap/css/bootstrap.css');
+                    </style>
+                    <li class="list-group-item mb-2">
+                        <div class="d-flex justify-content-between">
+                            <span>${this.nombrePlato}</span>
+                            <button type="button" class="btn btn-sm btn-danger botonQuitar">-</button>
+                            <span class="cantidad">${this.cantidad}</span>
+                            <button type="button" class="btn btn-sm btn-success botonAniadir">+</button>
+                            <span class="precio">${this.precioPlatoTotal} €</span>
+                        </div>
+                    </li>
+                `;
 
-          // Añadimos el contenido de la template (clonándola) al shadow DOM
-          this.shadow.appendChild(this.template.content.cloneNode(true));
+                // Añadimos el contenido de la template (clonándola) al shadow DOM
+                this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-          // Añadir el event listener para el botón
-          this.shadow.querySelector('.botonAniadir').addEventListener('click', () => {
-              this.cambiarCantidad(1);
-          });
+                // Añadir el event listener para el botón
+                this.shadowRoot.querySelector('.botonAniadir').addEventListener('click', () => {
+                    this.cambiarCantidad(1);
+                });
 
-          this.shadow.querySelector('.botonQuitar').addEventListener('click', () => {
-              this.cambiarCantidad(-1);
-          });
-      }
+                this.shadowRoot.querySelector('.botonQuitar').addEventListener('click', () => {
+                    this.cambiarCantidad(-1);
+                });
+            }
+        }
 
-      cambiarCantidad(incremento) {
-          let nuevaCantidad = parseInt(this.cantidad) + incremento;
+        cambiarCantidad(incremento) {
+            let nuevaCantidad = parseInt(this.cantidad) + incremento;
 
-          if (nuevaCantidad > 0) {
-              this.dispatchEvent(new CustomEvent('add-plato-del-mismo-tipo', {
-                  detail: {
-                      idPlato: this.idPlato,
-                      cantidadPlato: nuevaCantidad
-                  },
-                  bubbles: true,
-                  composed: true
-              }));
-          } else {
-              this.dispatchEvent(new CustomEvent('restar-plato-del-mismo-tipo', {
-                  detail: {
-                      idPlato: this.idPlato,
-                      cantidadPlato: nuevaCantidad
-                  },
-                  bubbles: true,
-                  composed: true
-              }));
-          }
-      }
+            if (nuevaCantidad > 0) {
+                this.dispatchEvent(new CustomEvent('add-plato-del-mismo-tipo', {
+                    detail: {
+                        idPlato: this.idPlato,
+                        cantidadPlato: nuevaCantidad
+                    },
+                    bubbles: true,
+                    composed: true
+                }));
+            } else {
+                this.dispatchEvent(new CustomEvent('restar-plato-del-mismo-tipo', {
+                    detail: {
+                        idPlato: this.idPlato,
+                        cantidadPlato: nuevaCantidad
+                    },
+                    bubbles: true,
+                    composed: true
+                }));
+            }
+        }
 
-      attributeChangedCallback(name, oldValue, newValue) {
-          if (name === 'data-cantidad-plato') {
-              this.cantidad = newValue;
-              this.shadow.querySelector('.cantidad').textContent = newValue;
-          }
-          if (name === 'data-precio-plato-total') {
-              this.precioPlatoTotal = newValue;
-              this.shadow.querySelector('.precio').textContent = newValue + " €";
-          }
-      }
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (name === 'data-cantidad-plato') {
+                this.cantidad = newValue;
+                this.shadowRoot.querySelector('.cantidad').textContent = newValue;
+            }
+            if (name === 'data-precio-plato-total') {
+                this.precioPlatoTotal = newValue;
+                this.shadowRoot.querySelector('.precio').textContent = newValue + " €";
+            }
+        }
 
-      static get observedAttributes() {
-          return ['data-cantidad-plato', 'data-precio-plato-total'];
-      }
-  }
+        static get observedAttributes() {
+            return ['data-cantidad-plato', 'data-precio-plato-total'];
+        }
+    }
 
-  customElements.define('plato-pedido-element', PlatoPedidoElement);
+    customElements.define('plato-pedido-element', PlatoPedidoElement);
 });
